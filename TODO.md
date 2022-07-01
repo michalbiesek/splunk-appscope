b585921d69b09f9d9817d2d6432316530dcc9a1b
michalbiesek <mbiesek@cribl.io>
2022-07-01 13:07:30 +0200

    Add experiment for splunk and sshd

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO.md
+
+### Todo
+
+- [ ] Add Lookup layer in cribl for user name(Redis)
+- [ ] Check why the keys do not work from host
--- start.sh
+++ start.sh
@@ -18,6 +18,26 @@ copy_key () {
 }
 
 #######################################
+# Generate ssh key and copy public key to container
+# Arguments:
+#   Username
+#   Name of the docker container
+#######################################
+ssh_gen_copy_key () {
+    local user_name=$1
+    local output_key=$2:/home/${user_name}/.ssh/authorized_keys
+    local key_name=${user_name}_key
+    local pub_key=${key_name}.pub
+
+    echo "Generate key $key_name"
+
+    ssh-keygen -t rsa -b 4096 -f $key_name -P ""
+
+    echo "Copying public key $pub_key for user $user_name to $output_key"
+    docker cp $pub_key $output_key
+}
+
+#######################################
 # Copy certificate to specific docker container
 # Arguments:
 #   Name of the docker container
@@ -41,6 +61,8 @@ openssl req -newkey rsa:2048 -nodes -keyout $PRIVATE_KEY_NAME -x509 -days 365 -o
 copy_key "cribl01"
 copy_cert "cribl01"
 copy_cert "appscope01_tls"
+ssh_gen_copy_key "foouser" "appscope03_ssh_test" 
+ssh_gen_copy_key "baruser" "appscope03_ssh_test" 
 
 printf '\n'
 echo "Demo is ready."
@@ -49,3 +71,10 @@ echo "To start scoping bash session run: 'docker-compose run appscope01'"
 echo "To start scoping individual commands run: 'docker exec -it appscope02 bash' and use ldscope/scope"
 echo "To use TLS:"
 echo "To start scoping individual commands run: 'docker exec -it appscope01_tls bash' and use ldscope/scope"
+echo "To use SSHD:"
+echo "To start scoping individual commands run: 'docker exec -it appscope_sshd bash' and use ldscope/scope"
+echo "To connect via ssh to appscope_sshd:"
+echo "To start scoping individual commands run: 'docker exec -it appscope_sshd bash' and use ldscope/scope"
+echo "'ssh foouser@localhost -i foouser_key -p 2022' pass:foouser"
+echo "'ssh baruser@localhost -i baruser_key -p 2022' pass:baruser"
+##TODO fix above - verify why keys do not work
